000a71fb933e58626cfa102919afe33ab38c586d
pinigor1974 <pinigor1974@gmail.com>
2025-01-14 19:38:29 +0300

    a: a

---
+++ hw05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -0,0 +1,42 @@
+package ru.otus.hw.repositories;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Author;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcAuthorRepository implements AuthorRepository {
+
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Author> findAll() {
+
+        return jdbcTemplate.query("select id, fullname from authors", (rs, rowNum) -> {
+            return Author.builder()
+                    .id(rs.getLong("id"))
+                    .fullName(rs.getString("full"))
+                    .build();
+        });
+    }
+
+    @Override
+    public Optional<Author> findById(long id) {
+
+        return Optional.ofNullable(jdbcTemplate.queryForObject("select id, fullname from authors where id = :id",
+                Map.of("id", id),
+                Author.class));
+    }
+
+}
