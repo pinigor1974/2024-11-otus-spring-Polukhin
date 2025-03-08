47401b99b162bac5490a1e89c3236e3d9513a113
pinigor1974 <pinigor1974@gmail.com>
2025-03-08 21:16:15 +0300

    add

---
+++ hw06/src/main/java/ru/otus/hw/models/Author.java
@@ -0,0 +1,16 @@
+package ru.otus.hw.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Author {
+    private long id;
+
+    private String fullName;
+}
