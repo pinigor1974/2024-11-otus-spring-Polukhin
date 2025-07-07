e5a2543556542cbf856e2a84ee0f6125586ed6f4
pinigor1974 <pinigor1974@gmail.com>
2025-07-07 20:42:33 +0300

    add

--- hw16/src/main/java/ru/otus/hw/indicators/BookRecordsHealthIndicator.java
+++ hw16/src/main/java/ru/otus/hw/indicators/BookRecordsHealthIndicator.java
@@ -19,8 +19,8 @@ public class BookRecordsHealthIndicator extends AbstractHealthIndicator {
     @Override
     protected void doHealthCheck(Health.Builder builder) {
         var numOfBookRecords = getNumBookRecords();
-        if(numOfBookRecords > 0) {
-            builder.withDetail("books records",numOfBookRecords ).status("Up").build();
+        if (numOfBookRecords > 0) {
+            builder.withDetail("books records",numOfBookRecords).status("Up").build();
         } else {
              builder.withDetail("books records is ", 0).status("NO Records Found").down().build();
         }
